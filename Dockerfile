dfe3012a012fa7dad0cd6652092cd07150d16fc4
Phuoc Nguyen <ba.phuoc.nbp@gmail.com>
2017-08-10 20:32:20 +1000

    change docker build

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,2 @@
-FROM circleci/node:8
+FROM circleci/node:8.2-browsers
 RUN sudo apt-get install -y rsync awscli
--- push.sh
+++ push.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker push arbornetworks-docker-public-docker.bintray.io/circleci-node-aws:8
+docker push nbphuoc/node-8-aws-browser
